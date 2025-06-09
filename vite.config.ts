fe36badb836cd5ea219c64212f4b387dd29cc0c2
Ihor Barban <barbanihor@gmail.com>
2025-06-09 18:58:04 +0200

    something done

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://obminbook.us-east-1.elasticbeanstalk.com',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
