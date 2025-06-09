fa33bf09d232bc85db2b1cd0b9c6a79653e6367d
Ihor Barban <barbanihor@gmail.com>
2025-06-09 15:40:37 +0200

    Vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tsconfigPaths(), // підтримка alias з tsconfig
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, 'src'), // можеш залишити для надійності, але плагін підхопить із tsconfig
     },
   },
   server: {
