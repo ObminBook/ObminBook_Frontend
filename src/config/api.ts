f1d4923b426924caa59182512aebba16ecd997a8
Ihor Barban <barbanihor@gmail.com>
2025-06-10 18:54:29 +0200

    https -> http

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,1 +1,1 @@
-export const API_BASE = 'https://obminbook.us-east-1.elasticbeanstalk.com';
+export const API_BASE = 'http://obminbook.us-east-1.elasticbeanstalk.com';
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import * as path from 'path';
 
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
   return {
     plugins: [react(), tsconfigPaths()],
     resolve: {
@@ -12,6 +12,18 @@ export default defineConfig(() => {
       },
       extensions: ['.ts', '.tsx', '.js', '.jsx'],
     },
+    ...(command === 'serve' && {
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://obminbook.us-east-1.elasticbeanstalk.com',
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
+      },
+    }),
     build: {
       rollupOptions: {
         external: () => false,
