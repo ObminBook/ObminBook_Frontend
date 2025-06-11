2ffe74ca65ebba5b0158ae23ed66f714d67dfc9d
Ihor Barban <barbanihor@gmail.com>
2025-06-11 13:20:36 +0200

    cretead resetstyle file

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './reduxStore/store';
+import './styles/reset.scss';
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
