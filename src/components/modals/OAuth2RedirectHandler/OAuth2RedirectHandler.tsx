5d6b6e5b6f9c69e6322394b4fee41d98bab48b3e
Ihor Barban <barbanihor@gmail.com>
2025-06-21 14:23:47 +0200

    oauth

--- src/components/modals/OAuth2RedirectHandler/OAuth2RedirectHandler.tsx
+++ src/components/modals/OAuth2RedirectHandler/OAuth2RedirectHandler.tsx
@@ -1,20 +1,21 @@
-import { checkAuth } from '@/features/authSlice/authSlice';
-import { useAppDispatch } from '@/reduxHooks/useAppDispatch';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '@/reduxHooks/useAppDispatch';
+import { checkAuth } from '@/features/authSlice/authSlice';
 
 const OAuth2RedirectHandler = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
-  console.log('OAuth2RedirectHandler rendered');
-
   useEffect(() => {
+    console.log('fetchUser started');
     const fetchUser = async () => {
       try {
         await dispatch(checkAuth()).unwrap();
+        console.log('checkAuth success');
         navigate('/search');
-      } catch {
+      } catch (err) {
+        console.error('checkAuth error', err);
         navigate('/login');
       }
     };
