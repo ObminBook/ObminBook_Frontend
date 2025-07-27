dbf9f44b6ff2be641140ab3a384708cb8b06fea7
Ihor Barban <barbanihor@gmail.com>
2025-07-27 13:09:56 +0200

    changed url

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(({ command }) => {
       server: {
         proxy: {
           '/api': {
-            target: 'http://3.218.8.20',
+            target: 'http://obminbook.eu-central-1.elasticbeanstalk.com/',
             changeOrigin: true,
             secure: false,
             rewrite: (path) => path.replace(/^\/api/, ''),
